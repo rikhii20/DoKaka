fceffa16f69f260032380d8e4c44604adfe42f2e
rikhii20 <rikhisobari22@gmail.com>
2022-03-04 17:11:58 +0800

    + cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const routes = require("./routes");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
